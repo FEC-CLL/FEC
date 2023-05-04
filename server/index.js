58e14effa6b9a5eebe54dd68af651e8dae460172
Frank Mirando <mirandofrank@gmail.com>
2023-05-04 12:28:26 -0700

    Included a font for entire app (#9)

--- server/index.js
+++ server/index.js
@@ -13,7 +13,18 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+// ROUTES
+
+// Product Reviews
+
+// Q & A
+
+// Related Products
+// https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40348/related
+
+
 const PORT = process.env.PORT || 3000; // <-- 8080 is also common
 app.listen(PORT, () => {
   console.log(`Server available at http://localhost${PORT}`);
 });
+
