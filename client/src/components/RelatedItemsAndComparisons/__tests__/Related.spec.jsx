865b830a2d04854b820113d9f0ee41a48d5526f8
Frank <mirandofrank@gmail.com>
2023-05-09 20:23:10 -0700

    Created two tests for my components.

--- client/src/components/RelatedItemsAndComparisons/__tests__/Related.spec.jsx
+++ client/src/components/RelatedItemsAndComparisons/__tests__/Related.spec.jsx
@@ -5,6 +5,11 @@ import RelatedItemsAndComContainer from '../RelatedItemsAndComContainer';
 describe('Related Products and Outfits', () => {
   test('Should render related products list', () => {
     render(<RelatedItemsAndComContainer />);
-    expect(screen.getByText('Related Products and Outfits')).toBeInTheDocument();
+    expect(screen.getByText('Related Products')).toBeInTheDocument();
   });
+  test('Should render outfits list', () => {
+    render(<RelatedItemsAndComContainer />);
+    expect(screen.getByText('Outfits')).toBeInTheDocument();
+  });
+
 });
