99ebf946d1f6ac5f9512c43a77f734f27e6e1c79
mellava <6126925+mellava@users.noreply.github.com>
2023-05-10 22:43:23 -0700

    Add dynamic quantity. Add dynamic price.

--- client/src/components/Overview/Product.jsx
+++ client/src/components/Overview/Product.jsx
@@ -1,39 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
-export default function Product({initProd = {}}) {
-  const {id, name, slogan, description, category, results, features} = initProd
+export default function Product({ product = {} }) {
+  const {
+    name, category, results,
+  } = product;
 
-  const [styleIndex, setStyleIndex] = useState(0)
-  const [mainImageIndex, setMainImageIndex] = useState(0)
+  const [styleIndex, setStyleIndex] = useState(0);
+  const [mainImageIndex, setMainImageIndex] = useState(0);
+  const [skuId, setSkuId] = useState(null);
 
-  if(!results?.length) {
-    return null
+  if (!results?.length) {
+    return null;
   }
 
-  const styleSkus = Object.keys(results[styleIndex].skus)
-  // console.log('obj', results[styleIndex].skus)
-  console.log('styleskus', styleSkus)
-  // console.log('styleskus 0 quantity', styleSkus[0].quantity)
-  //const quantityArray = [...Array(results[styleIndex].skus[0]).keys()]
-  // const quantityArray = [...Array(8).keys()]
-  const quantityArray = results[styleIndex].styleSkus
-  console.log('quantity array', quantityArray)
+  const styleSkus = Object.keys(results[styleIndex].skus);
 
   return (
     <div className="container">
       <div className="image-gallery">
         <div className="image-gallery__thumbnail-nav">
           <button type="button" className="image-gallery__thumbnail-nav__up" aria-label="Up" />
-          <ul>
-            {results[styleIndex].photos?.map((photo, index) => <li onClick={() => setMainImageIndex(index)}><img className="image-gallery__thumbnail-nav__image" src={photo.thumbnail_url} alt="Thumbnail 1" /></li>)}
+          <ul className="image-gallery__thumbnail-nav__list">
+            {results[styleIndex].photos?.map((photo, index) => <li><button onClick={() => setMainImageIndex(index)} onKeyPress={() => setMainImageIndex(index)} type="button" key={photo.thumbnail_url}><img className="image-gallery__thumbnail-nav__image" src={photo.thumbnail_url} alt="Thumbnail 1" /></button></li>)}
           </ul>
           <button type="button" className="image-gallery__thumbnail-nav__down" aria-label="Down" />
         </div>
         <div className="image-gallery__image">
-          <button type="button" className="image-gallery__image-left" aria-label="Left" onClick={() => setMainImageIndex(Math.max(0, mainImageIndex-1))}/>
+          <button type="button" className="image-gallery__image-left" aria-label="Left" onClick={() => setMainImageIndex(Math.max(0, mainImageIndex - 1))} />
           <img className="image-gallery__image__main" src={results[styleIndex].photos[mainImageIndex].url} alt="Main Product" />
-          <button type="button" className="image-gallery__image-right" aria-label="Right" onClick={() => setMainImageIndex(Math.min(mainImageIndex+1, results[styleIndex].photos.length-1))}/>
+          <button type="button" className="image-gallery__image-right" aria-label="Right" onClick={() => setMainImageIndex(Math.min(mainImageIndex + 1, results[styleIndex].photos.length - 1))} />
         </div>
       </div>
       <div className="product-information">
@@ -51,9 +47,14 @@ export default function Product({initProd = {}}) {
         <p className="product-information__category">{category}</p>
         <h2 className="product-information__product-title">{name}</h2>
         <p className="product-information__price">
-          <span className="product-information__price--strike">{results[styleIndex].original_price}</span>
-          {' '}
-          <span className={results[styleIndex].sale_price && "product-information__price--sale"}>{results[styleIndex].sale_price ? results[styleIndex].sale_price : null}</span>
+          {results[styleIndex].sale_price ? (
+            <span>
+              <span className="product-information__price--strike">{results[styleIndex].original_price}</span>
+
+              <span className="product-information__price--sale">{results[styleIndex].sale_price}</span>
+            </span>
+          )
+            : results[styleIndex].original_price}
         </p>
 
         <div className="product-information__style-selector">
@@ -62,17 +63,28 @@ export default function Product({initProd = {}}) {
             {results[styleIndex].name}
           </p>
           <ul>
-            {results.map((result, index) => <li onClick={() => setStyleIndex(index)}><span className="product-information__style-selector--selected" /><img className="product-information__style-selector__thumbnail" src={result.photos[0].thumbnail_url} alt="Add to Cart Icon" /></li>)}
+            {results.map((result, index) => (
+              <li>
+                <button type="button" onClick={() => setStyleIndex(index)} key={result.photos[0].thumbnail_url}>
+                  <span className="product-information__style-selector--selected" />
+                  <img className="product-information__style-selector__thumbnail" src={result.photos[0].thumbnail_url} alt="Add to Cart Icon" />
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="product-information__options">
-          <select className="product-information__select product-information__select--size" name="size" id="size">
-            {styleSkus.map(sku => <option value={results[styleIndex].skus[sku].size}>{results[styleIndex].skus[sku].size}</option>)}
+          <select className="product-information__select product-information__select--size" name="size" id="size" onChange={(e) => setSkuId(e.target.value)}>
             <option value="">Select size</option>
+            {styleSkus.map((sku) => (
+              <option value={sku} key={sku}>{results[styleIndex].skus[sku].size}</option>
+            ))}
           </select>
           <select className="product-information__select product-information__select--quantity" name="quantity" id="quantity">
-            {styleSkus.map(sku => <option value="1">{results[styleIndex].skus[sku].quantity}</option>)}
             <option value="">Select quantity</option>
+            {[...Array(results[styleIndex].skus[skuId]?.quantity || 1).keys()].map((number) => (
+              <option value={number + 1}>{number + 1}</option>
+            ))}
           </select>
         </div>
         <button type="button" className="product-information__add-to-cart">
