2e503d7efb97f6ebe6c4d0209186f5a54922bf6e
Frank <mirandofrank@gmail.com>
2023-05-11 15:52:15 -0700

    Added some basic functionalities for clicking on star button and clicking on product cards.

--- client/src/components/RelatedItemsAndComparisons/Card.jsx
+++ client/src/components/RelatedItemsAndComparisons/Card.jsx
@@ -5,14 +5,23 @@ import React from 'react';
 // TODO: import image from API request
 const Card = ({name, category, price, tempImg}) => {
 
-  const handleCardClick = () => {
+  const handleCardClick = (event) => {
+    event.preventDefault();
     alert('New product chosen');
     // TODO: This will change the current chosen product
   }
 
+  const handleCompareClick = (event) => {
+    event.stopPropagation();
+    alert('Comparing current product with chosen product');
+    // TODO: when a user clicks on the star, a modal table should pop up
+    // comparing current product to selected product
+  }
+
+  // TODO: replace button with star icon
   return (
     <div className="productCard" onClick={handleCardClick}>
-      <button className="compareButton">Star</button>
+      <button className="compareButton" onClick={handleCompareClick} >Star</button>
       <img className="cardImg" src={tempImg}></img>
       {category}
       <h3 className="prodName">{name}</h3>
