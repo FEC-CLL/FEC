b610e7f32b9ff5a48adb36874da3cf9c15ace4fb
Frank Mirando <mirandofrank@gmail.com>
2023-05-11 22:20:17 -0700

    Update Outfits.jsx
    another attempt to fix these darn lint errors....

--- client/src/components/RelatedItemsAndComparisons/Outfits.jsx
+++ client/src/components/RelatedItemsAndComparisons/Outfits.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 
-function Outfits(){
-  // Thought: I'm also probably going to need another html section 
-  // where it maps thru a user's outfit list and renders each product card
-
-//   const handleAddToOutfit = (event) => {
-//     event.preventDefault();
-//     alert('Current product added to outfit');
-//   };
-
+function Outfits() {
   return (
     <div className="addToOutfit">
       <h3>Add To Outfit</h3>
