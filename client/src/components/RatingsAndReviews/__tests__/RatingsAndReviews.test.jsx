2304c75b90328035535c2aa7e56a0b423d6300df
Julie Krav <j.krav07@gmail.com>
2023-05-12 23:35:18 -0700

    adding axios mock to the test

--- client/src/components/RatingsAndReviews/__tests__/RatingsAndReviews.test.jsx
+++ client/src/components/RatingsAndReviews/__tests__/RatingsAndReviews.test.jsx
@@ -1,11 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
 import RatingsAndReviewsContainer from '../RatingsAndReviewsContainer';
 
-describe('Renders Ratings & Review component to frontend', () => {
-  const testData = {
-    id: 40344,
-  };
+const testData = {
+  id: 40344,
+};
 
+jest.mock('axios');
+axios.get.mockResolvedValue({ data: testData });
+
+describe('Renders Ratings & Review component to frontend', () => {
   it('should render title', () => {
     render(<RatingsAndReviewsContainer initProd={testData} />);
     const title = screen.getByText('RATINGS & REVIEWS');
