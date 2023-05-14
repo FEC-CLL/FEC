31331505c421aaf9d33038d7352f9da9df7e36c8
Frank Mirando <mirandofrank@gmail.com>
2023-05-13 18:02:14 -0700

    Update RelatedProducts.jsx
    Fixing lint errors

--- client/src/components/RelatedItemsAndComparisons/RelatedProducts.jsx
+++ client/src/components/RelatedItemsAndComparisons/RelatedProducts.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Card from './Card';
-import products from '../../../../sampleData/products.json';
 
 // Need to have max number of card rendered between 3-4
-// Would I need to send an API request for product images as well?
-// Probably need to send another API request; retrieve the image for the default style for each product
+// Need to send another API request to retrieve images
 
-
-function RelatedProducts({relatedProducts}) {
+function RelatedProducts({ relatedProducts }) {
   const tempImg = 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80';
   return (
     <div className="relatedList" style={{ display: 'flex' }}>
