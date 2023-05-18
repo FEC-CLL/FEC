d5b552da6e46c796bdc4c0349fa80b6e2785af9a
Melissa Lava <6126925+mellava@users.noreply.github.com>
2023-05-18 13:09:32 -0700

    Product expanded view (#61)
    * Add zoom to main image
    
    * Add collapse and expand with zoom for main image
    
    * Add expand and collapse functionality to image. Add highlight to selected thumbnail image
    
    * Fix product test. Modify jest config file to omit related products test
    
    * Fix unique keys

--- client/src/components/Overview/ProductStar.jsx
+++ client/src/components/Overview/ProductStar.jsx
@@ -4,19 +4,19 @@ export default function ProductStar({ averageReview }) {
   const stars = [];
   let i = averageReview;
   for (; i >= 1; i -= 1) {
-    stars.push(<span title="filled star" className="product-information__reviews--4" />);
+    stars.push(<span title="filled star" className="product-information__reviews--4" key={`${i} filled`} />);
   }
   if (i > 0 && i < 0.25) {
-    stars.push(<span title="one quarter star" className="product-information__reviews--1" />);
+    stars.push(<span title="one quarter star" className="product-information__reviews--1" key={i} />);
   }
   if (i >= 0.25 && i < 0.75) {
-    stars.push(<span title="half star" className="product-information__reviews--2" />);
+    stars.push(<span title="half star" className="product-information__reviews--2" key={i} />);
   }
   if (i >= 0.75 && i < 1) {
-    stars.push(<span title="three quarter star" className="product-information__reviews--3" />);
+    stars.push(<span title="three quarter star" className="product-information__reviews--3" key={i} />);
   }
   for (let j = (5 - averageReview); j >= 1; j -= 1) {
-    stars.push(<span title="unfilled star" className="product-information__reviews--0" />);
+    stars.push(<span title="unfilled star" className="product-information__reviews--0" key={`${j} unfilled`} />);
   }
   return stars;
 }
