78ee7d4a6db2377e598f77fb54ddac8e7cae9df5
mellava <6126925+mellava@users.noreply.github.com>
2023-05-18 02:17:02 -0700

    Add react router. Refactor App and modify App test. Create navbar component and styles

--- client/src/components/__tests__/App.spec.jsx
+++ client/src/components/__tests__/App.spec.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
-import axios from 'axios';
+import {
+  useLoaderData,
+} from 'react-router-dom';
 import App from '../App';
 import mockProduct from '../../../../sampleData/product.json';
 import Product from '../Overview/Product';
@@ -8,18 +10,22 @@ import RelatedItems from '../RelatedItemsAndComparisons/RelatedItemsAndComContai
 import QandA from '../QandA/QandAContainer';
 import Ratings from '../RatingsAndReviews/RatingsAndReviewsContainer';
 
-jest.mock('axios');
 jest.mock('../Overview/Product');
 jest.mock('../RelatedItemsAndComparisons/RelatedItemsAndComContainer');
 jest.mock('../QandA/QandAContainer');
 jest.mock('../RatingsAndReviews/RatingsAndReviewsContainer');
-axios.get.mockResolvedValue({ data: mockProduct });
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLoaderData: jest.fn(),
+}));
+
+useLoaderData.mockResolvedValue(mockProduct);
 
 describe('App', () => {
   test('renders App', async () => {
     render(<App />);
     await waitFor(() => {
-      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(useLoaderData).toHaveBeenCalledTimes(1);
       expect(Product).toHaveBeenCalledTimes(1);
       expect(RelatedItems).toHaveBeenCalledTimes(1);
       expect(QandA).toHaveBeenCalledTimes(1);
