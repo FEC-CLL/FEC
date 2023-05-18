99683f88f5be4a36415c3d89c68baecf819ed28e
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2023-05-18 13:32:25 -0700

    added a filter by seller, and added report question functionality, fixed a little css for add answer and add question, fix add image to add answer bug

--- client/src/components/QandA/AddQuestion.jsx
+++ client/src/components/QandA/AddQuestion.jsx
@@ -40,22 +40,25 @@ function AddQuestion({
           <h4 className="modal-title">Ask Your Question</h4>
           <div className="modal-subtitle">
             About the
+            {' '}
             {product.name}
           </div>
         </div>
-        <form onSubmit={handleSubmit} className="modal-body">
-          <div className="modal-body-title">* Your Question: </div>
-          <textarea onChange={handleQuestionChange} placeholder="Why did you like the product or not?" maxLength="1000" className="modal-textArea" required />
-          <div className="modal-body-container">
-            <div className="modal-component-title">* What is your nickname:</div>
-            <input onChange={handleNicknameChange} type="text" maxLength="60" placeholder="Example: jackson11!" required />
+        <form onSubmit={handleSubmit}>
+          <div className="modal-body">
+            <div className="modal-body-title">* Your Question: </div>
+            <textarea onChange={handleQuestionChange} placeholder="Why did you like the product or not?" maxLength="1000" className="modal-textArea" required />
+            <div className="modal-body-container">
+              <div className="modal-component-title">* What Is Your Nickname:</div>
+              <input onChange={handleNicknameChange} type="text" maxLength="60" placeholder="Example: jackson11!" required />
+            </div>
+            <div className="modal-warning">*For privacy reasons, do not use your full name or email address </div>
+            <div className="modal-body-container">
+              <div className="modal-component-title">* Your Email:</div>
+              <input onChange={handleEmailChange} id="email" type="email" maxLength="60" placeholder="Example: jack@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
+            </div>
+            <div className="modal-warning">*For authentication reasons, you will not be emailed </div>
           </div>
-          <div className="modal-warning">*For privacy reasons, do not use your full name or email address </div>
-          <div className="modal-body-container">
-            <div className="modal-component-title">* Your Email:</div>
-            <input onChange={handleEmailChange} id="email" type="email" maxLength="60" placeholder="Example: jack@email.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
-          </div>
-          <div className="modal-warning">*For authentication reasons, you will not be emailed </div>
           <div className="modal-footer">
             <button type="button" onClick={() => setShow(!show)} className="button">Close</button>
             <button className="button" type="submit">Submit</button>
